c38388a7e960cb2efbd4dd587d36315ceb1b728c
geekplux <geekplux@qq.com>
2017-07-08 11:26:03 +0800

    prepare deploy

--- poi.config.js
+++ poi.config.js
@@ -2,6 +2,8 @@ module.exports = {
   autoprefixer: {
     browsers: ['ie > 8', 'last 4 versions']
   },
+  // vendor: false,
+  minimize: false
   // webpack (config) {
   //   config.target = 'node'
   //   return config
